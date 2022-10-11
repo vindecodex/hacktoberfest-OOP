a9ae8aff68e3e73644d8acca9f02fbd1bea7a7ca
Vincent Villaluna <villalunavincent@gmail.com>
2022-10-11 13:12:38 +0800

    sub-class: Square subclass of Shape

---
+++ sub-class/shape/square.ts
@@ -0,0 +1,11 @@
+import { Shape } from '../../base/shape';
+
+export class Square extends Shape {
+				constructor(sides: number = 0, length: number = 0) {
+								super(sides, length, length, 'square');
+				}
+
+				getArea():number {
+								return this.getLength() * this.getHeight();
+				}
+}
