226f8a5172e1830524317aedf0c3b293e2c29eca
Vincent Villaluna <villalunavincent@gmail.com>
2022-10-12 09:38:59 +0800

    Tests: Example test cases

---
+++ base/shape.spec.ts
@@ -0,0 +1,24 @@
+import { Shape } from './shape';
+
+describe('Shape', () => {
+				it('should return fields passed to the shape', () => {
+								// Setup
+								const expected = {
+												sides: 4,
+												length: 10,
+												height: 20,
+												name: 'Rectangle',
+								}
+								const shape = new Shape(
+												expected.sides,
+												expected.length,
+												expected.height,
+												expected.name,
+								);
+
+								expect(expected.sides).toEqual(shape.getSides());
+								expect(expected.length).toEqual(shape.getLength());
+								expect(expected.height).toEqual(shape.getHeight());
+								expect(expected.name).toEqual(shape.getName());
+				})
+})
